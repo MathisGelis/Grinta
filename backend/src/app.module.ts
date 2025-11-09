d7c47165301e23251469f8c036d939bc5ebf71eb
MathisGelis <mathis.gelis@epitech.eu>
2025-11-08 20:14:39 -0500

    add: jwt token route protection + exception filter

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { UserLoginDto } from './dto/user-login.dto';
+
+@ApiTags('auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @ApiOperation({ summary: 'Login user and get JWT token' })
+  @ApiResponse({ status: 200, description: 'Returns JWT token' })
+  @ApiResponse({ status: 401, description: 'Invalid credentials' })
+  async login(@Body() body: UserLoginDto) {
+    const user = await this.authService.validateUser(body.email, body.password);
+
+    if (!user)
+        throw new UnauthorizedException('Invalid credentials');
+    return this.authService.login(user);
+  }
+}
